5394f64427c2dcfd4a08b9849bf17440dd64b2c4
XX <fallwall19@gmail.com>
2019-09-02 17:51:09 -0400

    tile2 setinterval with color

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Memphis @ Labor Day Weekend 2019
 
 ![Memphis Picture](https://i.imgur.com/BdGMNBT.jpg)
+
+
+### Setting interval with Hooks (useEffect) 💡
+https://upmostly.com/tutorials/setinterval-in-react-components-using-hooks
---
+++ src/components/Tile2.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import './Tile2.css';
+
+const colors = [
+  '#212436',
+  '#4458A0',
+  '#BD098E',
+  '#CD2D48',
+  '#F1C40F',
+  '#672394',
+  '#F725A0',
+  '#FAD141',
+  '#0CB2C0',
+  '#E8E6D9'
+];
+
+export default function Tile2() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('This will run 3s!');
+      const randNum = Math.floor(Math.random() * 25);
+      document.getElementById(randNum).setAttribute('style', `background-color: ${colors[Math.floor(Math.random() * colors.length)]} `);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="tiles-container">
+      {Array(25).fill("memphis").map((m, i) =>
+        <div
+          key={i}
+          className="container"
+          id={i}
+          style={{ backgroundColor: colors[Math.floor(Math.random() * colors.length)] }}
+        >
+          <div className="memphis-blue"></div>
+          <div className="middle">
+            <div className="text">MEMPHIS</div>
+          </div>
+        </div>)}
+    </div>
+  )
+}
