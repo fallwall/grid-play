245aa0602e885d751f62d7b1eddd969ea6ee90c8
XX <fallwall19@gmail.com>
2019-09-02 16:58:03 -0400

    tile1 done

--- src/components/Tile1.jsx
+++ src/components/Tile1.jsx
@@ -5,7 +5,14 @@ export default function Tile1() {
   return (
     <div className="tile-container">
       {Array(25).fill("tile").map((t, i) =>
-        <div key={i} className={i % 2 === 0 ? "black tile" : "grey tile"}></div>
+        <div key={i} className={i % 2 === 0 ? "black tile-block" : "grey tile-block"}>
+          <div className="tile"></div>
+          <div className="tile-block-hover">
+            <div className="tile-text">
+              Memphis
+            </div>
+          </div>
+        </div>
       )}
     </div>
   )
