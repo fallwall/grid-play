5cf3de27140a801c85e7f1a1001a00f28c9ff960
XX <fallwall19@gmail.com>
2019-09-02 19:46:48 -0400

    tile3 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import Tile1 from './components/Tile1';
 import Tile2 from './components/Tile2';
+import Tile3 from './components/Tile3';
 import { Link, Route } from 'react-router-dom';
 import './App.css';
 
@@ -10,13 +11,15 @@ export default class App extends Component {
     return (
       <div>
 
-        <Link exact="true" to="/">Home</Link>
+        <Link exact to="/">Home</Link>
         <Link to="/tile1">Tile1</Link>
         <Link to="/tile2">Tile2</Link>
+        <Link to="/tile3">Tile3</Link>
 
         <Route path="/" render={() => <Home />} />
         <Route path="/tile1" render={() => <Tile1 />} />
         <Route path="/tile2" render={() => <Tile2 />} />
+        <Route path="/tile3" render={() => <Tile3 />} />
       </div>
     )
   }
---
+++ src/components/Tile3.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import './Tile3.css';
+
+const content = [
+  {
+    url: 'https://i.imgur.com/E41YjEo.jpg',
+    title: 'Airport - MEM',
+    caption: 'Where car rental ran out of cars at night.'
+  },
+  {
+    url: 'https://i.imgur.com/GN7rqQE.jpg',
+    title: 'Hattie B\'s @ Midtown',
+    caption: 'Still the best fried chicken on this fing🌎. (Had it in Birmingham, AL before.)'
+  },
+  {
+    url: 'https://i.imgur.com/FAQpLxW.jpg',
+    title: 'Hattie B\'s @ Midtown',
+    caption: 'Note to myself: only get mild. This medium heat is not at my level.'
+  },
+  {
+    url: 'https://i.imgur.com/lBInxtn.jpg',
+    title: 'Tennessee State Line',
+    caption: 'Entering Mississippi. O Brother, Where Art Thou?'
+  },
+  {
+    url: 'https://i.imgur.com/FVRxJGC.jpg',
+    title: 'Pyramid',
+    caption: 'Fun Fact: Memphis was named after the ancient capital of Egypt on the Nile River.'
+  },
+  {
+    url: 'https://i.imgur.com/VYuSLno.jpg',
+    title: 'Bass Pro @ Pyramid',
+    caption: 'Most extensive firearm selection I\'ve ever seen.'
+  },
+  {
+    url: 'https://i.imgur.com/xthRiOo.jpg',
+    title: 'Children @ Bass Pro @ Pyramid',
+    caption: 'While parents picking their toys, children can practice on shooting games arcade stuff and whatever you call this.'
+  },
+  {
+    url: 'https://i.imgur.com/MMEf5zl.jpg',
+    title: 'Beale Street',
+    caption: 'Heart of tourism. Kinda like Nola. Kinda like Nashville.'
+  },
+  {
+    url: 'https://i.imgur.com/9faYSlj.jpg',
+    title: 'Sign @ Beale Street',
+    caption: '💕🖤💕🖤💕🖤💕🖤💕'
+  },
+  {
+    url: 'https://i.imgur.com/CgNO79o.jpg',
+    title: 'Half Dome Entrance of Daisy @ Beale Street',
+    caption: 'Some kind of historical-looking music vanue'
+  },
+  {
+    url: 'https://i.imgur.com/gtDxUaj.jpg',
+    title: 'Mural @ Beale Street',
+    caption: 'My peak tourist moment'
+  },
+  {
+    url: 'https://i.imgur.com/5lcQdY3.jpg',
+    title: 'Handy Park @ Beale Street',
+    caption: 'small outdoor music venue'
+  },
+  {
+    url: 'https://i.imgur.com/aGznnuq.jpg',
+    title: 'Colorful Disco Place near Beale',
+    caption: 'This whole page was inspired by this'
+  },
+  {
+    url: 'https://i.imgur.com/TDuHBgb.jpg',
+    title: 'Colorful Disco Place near Beale',
+    caption: 'This whole page was inspired by this'
+  },
+  {
+    url: 'https://i.imgur.com/o2w1tlu.jpg',
+    title: 'Cocky\'s BBQ',
+    caption: 'Good ribs. And I really fell in love with their dinner roll. The secret is probably nothing but excessive amount of butter. But... damn.'
+  },
+  {
+    url: 'https://i.imgur.com/gQjiAsE.jpg',
+    title: 'Shiloh National Military Park',
+    caption: 'The guy next to me: \'We lost this one.\'😐😐😐'
+  },
+]
+
+export default class Tile3 extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    }
+  }
+
+  shuffle = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  componentDidMount() {
+    const images = this.shuffle(content);
+    this.setState({
+      images: images
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+
+        <ul id="categories" class="clr">
+          <li class="pusher"></li>
+          {this.state.images.map((image, i) =>
+            <li key={i}>
+              <div>
+                <img src={image.url} alt="" />
+                <h1>{image.title}</h1>
+                <p>{image.caption}</p>
+              </div>
+            </li>
+          )}
+
+          {/* <li class="pusher"></li>
+                <li class="pusher"></li> */}
+
+        </ul>
+      </div>
+    )
+  }
+}
+
