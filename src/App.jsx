acd98106a7d858ac4d03743b14c2a9e1007a49ba
XX <fallwall19@gmail.com>
2019-09-02 16:00:49 -0400

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Tiles from './components/Tiles';
+import Home from './components/Home';
+import Tile1 from './components/Tile1';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
-
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Tiles />
+
+        <Link exact to="/">Home</Link>
+        <Link exact to="/tile1">Tile1</Link>
+
+        <Route path="/" render={() => <Home />} />
+        <Route path="/tile1" render={() => <Tile1 />} />
       </div>
     )
   }
--- src/components/Tile1.jsx
+++ src/components/Tile1.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import "./Tiles.css";
+import "./Tile1.css";
 
-export default function Tiles() {
+export default function Tile1() {
   return (
     <div className="tile-container">
       {Array(25).fill("tile").map((t, i) =>
-        <div key={i} className={ i  % 2 === 0 ? "black tile" : "red tile"}></div>
+        <div key={i} className={i % 2 === 0 ? "black tile" : "grey tile"}></div>
       )}
     </div>
   )
