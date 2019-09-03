4d6479ebe997eb072192d1a3def4449574729134
XX <fallwall19@gmail.com>
2019-09-02 21:36:49 -0400

    author name lol

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,14 @@ export default class Home extends Component {
     return (
       <div className="home">
 
-
         <img src='https://i.imgur.com/8ZG2tjm.jpg' alt='beale st' style={{ width: "100vw"}} />
-
-
         <div className="content">
           <p>About that time I visited Memphis for the fist time.</p>
           <p>I was inspired by some colorful grids.</p>
           <p>Immediately realizing... In my head, I'm already coding this.</p>
         </div>
+
+        <div class="author">Tibby Xu @ Labor Day Weekend 2019</div>
 
       </div>
     )
