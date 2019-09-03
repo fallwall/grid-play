a9b945a35ee0595df944e18a3420d277c7c531f5
XX <fallwall19@gmail.com>
2019-09-02 20:43:47 -0400

    adjust for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,13 @@ export default class Home extends Component {
       <div className="home">
 
 
-        <img src='https://i.imgur.com/8ZG2tjm.jpg' alt='beale st' style={{ width: "100vw", marginTop: "-140vh"}} />
+        <img src='https://i.imgur.com/8ZG2tjm.jpg' alt='beale st' style={{ width: "100vw"}} />
 
 
         <div className="content">
           <p>About that time I visited Memphis for the fist time.</p>
           <p>I was inspired by some colorful grids.</p>
-          <p>Immediately realizing...</p>
-          <p>In my head, I'm already coding this.</p>
+          <p>Immediately realizing... In my head, I'm already coding this.</p>
         </div>
 
       </div>
